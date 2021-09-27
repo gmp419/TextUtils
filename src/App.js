84d6fb9734edd769d3807fc3dbd84fa577132a02
gaurang <patel7483g@gmail.com>
2021-09-27 20:31:03 -0300

    withour reload go to page

--- src/App.js
+++ src/App.js
@@ -3,49 +3,66 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import About from "./components/About";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Alert from "./components/Alert";
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 let name = "Gaurang";
 function App() {
-  const [mode, setMode] = useState('light');
+  const [mode, setMode] = useState("light");
   const [alert, setAlert] = useState(null);
 
-  const showAlert = (message,type) =>{
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
-      typ:type
-    })
-    setTimeout(() =>{
+      typ: type,
+    });
+    setTimeout(() => {
       setAlert(null);
     }, 3000);
-  }
+  };
 
-  const toggleMode = ()=>{
-    if(mode === 'light'){
-      setMode('dark');
-      document.body.style.backgroundColor = '#003366';
-      showAlert("Dark mode has been enabled","success");
-      document.title = 'TextUtils - Dark Mode';
-      }else{
-      setMode('light');
-      document.body.style.backgroundColor = '#ffffff';
-      showAlert("light mode has been enabled","success");
-      document.title = 'TextUtils - Dark Mode';
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#003366";
+      showAlert("Dark mode has been enabled", "success");
+      document.title = "TextUtils - Dark Mode";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "#ffffff";
+      showAlert("light mode has been enabled", "success");
+      document.title = "TextUtils - Dark Mode";
     }
-  }
+  };
 
   return (
     <>
-      
-      <Navbar title="Gaurang" home="My Home" about="About" mode={mode} enableDarkMode={toggleMode}/>
-      <Alert alert={alert}/>
-      <div className="container">
-      <TextForm heading='My Textutility' mode={mode} showAlert={showAlert} />
-      <About></About>
-      </div>
-      </>
+      <Router>
+        <Navbar
+          title="Gaurang"
+          home="My Home"
+          about="About"
+          mode={mode}
+          enableDarkMode={toggleMode}
+        />
+        <Alert alert={alert} />
+        <div className="container">
+          <Switch>
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route exact path="/">
+              <TextForm
+                heading="My Textutility"
+                mode={mode}
+                showAlert={showAlert}
+              />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </>
   );
 }
 
